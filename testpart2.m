68907590c9d8399eddaa3937a6e74552caf7c3ff
maneu96 <manueleusebiofreitas@gmail.com>
2019-12-18 17:05:28 +0000

    almost a acabar a funcao de ransac

--- testpart2.m
+++ testpart2.m
@@ -24,40 +24,48 @@ depth_array(isnan(depth_array))=0;
 points_2=f2(1:2,matches(2,:));
 xyz_I2= position3(Depth_cam.K,RGB_cam.K,R_d_to_rgb,T_d_to_rgb,depth_array,floor(points_2));
 
-%centroid calculations
-c_1=nanmean(xyz_I1,2);
-c_2=nanmean(xyz_I2,2);
-%pushing the centroids to the origin
-xyz_I1=bsxfun(@(a,b) a-b,xyz_I1,c_1);
-xyz_I2=bsxfun(@(a,b) a-b,xyz_I2,c_2);
-%remove nan from data
-xyz_I1(:,~all(~isnan(xyz_I1)))=[];
-xyz_I2(:,~all(~isnan(xyz_I2)))=[];
 
 
-E=xyz_I2*(xyz_I1)';
+%% Ransac
+%returns indexes of the inliers in the vector matches (which indexes all the features)  
+inliers=ransac_3dtransformation(xyz_I1,xyz_I2);
 
-[U,S,V]=svd(E);
-R=V*U';
-t=c_1-R*c_2;
 
-H= [R t;0 0 0 1];
-%% Show point clouds
-load depth_0000.mat
-depth_array(isnan(depth_array))=0;
- pc1=point_cloud(Depth_cam.K,RGB_cam.K,R_d_to_rgb,T_d_to_rgb,depth_array);
- pc1.Color=reshape(I1,[],3);
- showPointCloud(pc1);
- figure;
- load depth_0001.mat
- depth_array(isnan(depth_array))=0;
- pc2=point_cloud(Depth_cam.K,RGB_cam.K,R_d_to_rgb,T_d_to_rgb,depth_array);
- pc2.Color=reshape(I2,[],3);
- showPointCloud(pc2);
+
+% %centroid calculations
+% c_1=nanmean(xyz_I1,2);
+% c_2=nanmean(xyz_I2,2);
+% %pushing the centroids to the origin
+% xyz_I1=bsxfun(@(a,b) a-b,xyz_I1,c_1);
+% xyz_I2=bsxfun(@(a,b) a-b,xyz_I2,c_2);
+% %remove nan from data
+% xyz_I1(:,~all(~isnan(xyz_I1)))=[];
+% xyz_I2(:,~all(~isnan(xyz_I2)))=[];
 % 
-% %estimate 3d transformation between Point Clouds
 % 
-% %meanA=nanmean(pc1.Location);
-% %meanB=nanmean(pc2.Location);
-%   H_2=pcregrigid(pc2,pc1);
-%   H_2=H_2.T';
+% E=xyz_I2*(xyz_I1)';
+% 
+% [U,S,V]=svd(E);
+% R=V*U';
+% t=c_1-R*c_2;
+% 
+% H= [R t;0 0 0 1];
+% %% Show point clouds
+% load depth_0000.mat
+% depth_array(isnan(depth_array))=0;
+%  pc1=point_cloud(Depth_cam.K,RGB_cam.K,R_d_to_rgb,T_d_to_rgb,depth_array);
+%  pc1.Color=reshape(I1,[],3);
+%  showPointCloud(pc1);
+%  figure;
+%  load depth_0001.mat
+%  depth_array(isnan(depth_array))=0;
+%  pc2=point_cloud(Depth_cam.K,RGB_cam.K,R_d_to_rgb,T_d_to_rgb,depth_array);
+%  pc2.Color=reshape(I2,[],3);
+%  showPointCloud(pc2);
+% % 
+% % %estimate 3d transformation between Point Clouds
+% % 
+% % %meanA=nanmean(pc1.Location);
+% % %meanB=nanmean(pc2.Location);
+% %   H_2=pcregrigid(pc2,pc1);
+% %   H_2=H_2.T';
